a13c59e7819cae3d5d31a8976e51d8490894a48e
Ab <alzw7033@gmail.com>
2026-09-05 20:31:53 +0800

    Update

--- config/services.php
+++ config/services.php
@@ -4,25 +4,25 @@ return [
     'donation_web_service' => [
         'url' => env(
             'DONATION_WEB_SERVICE_URL',
-            'http://127.0.0.1:8000/api/webservice/donations/available'
+            'http://127.0.0.1:8888/api/webservice/donations/available'
         ),
     ],
     'food_request_web_service' => [
         'url' => env(
             'FOOD_REQUEST_WEB_SERVICE_URL',
-            'http://127.0.0.1:8000/api/webservice/requests/approved'
+            'http://127.0.0.1:8888/api/webservice/requests/approved'
         ),
     ],
     'delivery_web_service' => [
         'url' => env(
             'DELIVERY_WEB_SERVICE_URL',
-            'http://127.0.0.1:8000/api/webservice/deliveries/status'
+            'http://127.0.0.1:8888/api/webservice/deliveries/status'
         ),
     ],
     'user_profile_web_service' => [
         'url' => env(
             'USER_PROFILE_WEB_SERVICE_URL',
-            'http://127.0.0.1:8000/api/webservice/users'
+            'http://127.0.0.1:8888/api/webservice/users'
         ),
     ],
 ];
